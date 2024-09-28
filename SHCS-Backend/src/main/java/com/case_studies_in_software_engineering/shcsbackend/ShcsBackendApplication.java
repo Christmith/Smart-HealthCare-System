68c34bfeaadb0c5cd2a451ec41ff261649b057f6
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-09-29 00:25:08 +0530

    Initial Commit

---
+++ SHCS-Backend/src/main/java/com/case_studies_in_software_engineering/shcsbackend/ShcsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.case_studies_in_software_engineering.shcsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShcsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShcsBackendApplication.class, args);
+    }
+
+}
---
+++ SHCS-Backend/src/test/java/com/case_studies_in_software_engineering/shcsbackend/ShcsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.case_studies_in_software_engineering.shcsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShcsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
